5d4ce45c29a35dfe9562b58757bb7e0bc4e3901d
乐意分享 <WangCharlie@users.noreply.github.com>
2017-07-03 19:46:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+
+# generate fresh rsa key
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+
+# generate fresh dsa key
+ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+
+#prepare run dir
+mkdir -p /var/run/sshd
+
+#prepare sshd config
+ sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config \
+ && sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config \
+ && sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config \
+ && sed -i "s/#AuthorizedKeysFile/AuthorizedKeysFile/g" /etc/ssh/sshd_config \
+ && sed -i "s/#X11Forwarding no/X11Forwarding yes/g" /etc/ssh/sshd_config \
+ && sed -i "s/#PermitUserEnvironment no/PermitUserEnvironment yes/g" /etc/ssh/sshd_config \
+ && echo "ForwardX11Trusted yes" >> /etc/ssh/ssh_config
+
+#prepare xauth
+touch /root/.Xauthority
+
+# generate machine-id
+uuidgen > /etc/machine-id
+
+# set keyboard for all sh users
+echo "export QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /etc/profile
+# set keyboard for direct command use
+echo "QT_XKB_CONFIG_ROOT=/usr/share/X11/locale" >> /root/.ssh/environment
+
+source /etc/profile
+
 #start net speeder
 ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
 if [ "$ETH"  ==  '1' ] ; then
@@ -8,6 +40,6 @@ if [ "$ETH"  ==  '0' ] ; then
     nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 fi
 
-/etc/init.d/ssh restart
+/usr/sbin/sshd -D
 
-/usr/bin/python /ssr/shadowsocks/server.py "$@"
+/usr/sbin/python /ssr/shadowsocks/server.py "$@"
